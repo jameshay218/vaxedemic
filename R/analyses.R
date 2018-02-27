c60fa777932061320867f6ba3feff5efdf886780
Caroline Walters <c.e.walters6@gmail.com>
2018-02-27 08:18:13 +0000

    created functions for time end, global attack rate, and deaths which take simulation output as arguments

---
+++ R/analyses.R
@@ -0,0 +1,27 @@
+######################################################################
+# Functions for manipulating simulation output
+######################################################################
+
+# this is tend... I presume the final time?
+time_end <- function(results){
+  ncol(results$S)
+}
+
+# vector of deaths... What does each entry corresponds to? I presume split by
+#  coutry, risk group, age group?
+deaths <- function(results){
+  X - results$S[,tend] - results$SV[,tend] - results$E[,tend] - results$EV[,tend] -
+    results$I[,tend] - results$IV[,tend] - results$R[,tend] - results$RV[,tend]
+}
+
+# total number of deaths
+worldwide_deaths <- function(results){
+  sum(deaths(results))
+}
+
+# global attack rate
+global_attack <- function(results){
+  pop_total <- sum(X)
+  tend <- time_end(results)
+  sum(results$R[,tend] + results$RV[,tend] + deaths(results))/pop_total
+}

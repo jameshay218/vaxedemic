166ad178a6584baed813c21074662e7aee781a38
Caroline Walters <c.e.walters6@gmail.com>
2018-02-28 11:24:03 +0000

    corrected deaths function: X changed to popns

--- R/analyses.R
+++ R/analyses.R
@@ -60,9 +60,11 @@ time_end <- function(results){
 
 # vector of deaths... What does each entry corresponds to? I presume split by
 #  coutry, risk group, age group?
+
+# Check if need popns or X...
 deaths <- function(results){
   tend <- time_end(results)
-  X - results$S[,tend] - results$SV[,tend] - results$E[,tend] - results$EV[,tend] -
+  popns - results$S[,tend] - results$SV[,tend] - results$E[,tend] - results$EV[,tend] -
     results$I[,tend] - results$IV[,tend] - results$R[,tend] - results$RV[,tend]
 }
 

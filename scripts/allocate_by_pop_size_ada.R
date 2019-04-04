24c5aa325bd80a8496b4a05c8d5d165956cd4f3d
Ada Yan <a.yan@imperial.ac.uk>
2019-04-04 17:12:47 +0100

    fix for vaccinating China only caused really slow vaccine allocation when China was saturated -- rewind

--- scripts/allocate_by_pop_size_ada.R
+++ scripts/allocate_by_pop_size_ada.R
@@ -9,12 +9,7 @@ allocate_top_n_countries <- function(n_countries_allocated) {
   pop_size <- read.csv("data/demographic_data_intersect.csv", stringsAsFactors = FALSE)
   pop_size <- pop_size[order(pop_size$N, decreasing = TRUE),]
   countries_allocated <- pop_size[seq_len(n_countries_allocated), "countryID"]
-  if(n_countries_allocated == 1) {
-    new_coverage$dose_per_1000 <- 1e-6
-  } else {
-    new_coverage$dose_per_1000 <- 0
-  }
-
+  new_coverage$dose_per_1000 <- 0
   new_coverage[new_coverage$country %in% countries_allocated, "dose_per_1000"] <- 1000
   write.table(new_coverage, paste0("data/coverage_tables_by_popn/coverage_data_",n_countries_allocated,".csv"),sep=",",row.names=FALSE)
   invisible(new_coverage)
